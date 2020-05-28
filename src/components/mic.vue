20836265572c0cf95590687b79f7f014e22290ca
ch3njust1n <ch3njus@gmail.com>
2020-05-28 02:15:14 -0400

    Uppercase S in student

--- src/components/mic.vue
+++ src/components/mic.vue
@@ -29,7 +29,7 @@ export default {
 			prog: [
 				{"name": "Machine Intelligence Community, Inc.", "year": "2018-Present", "link":"https://machineintelligence.cc", "desc": "501(c)(3) nonprofit for making education on machine intelligence accessible, effective, and innovative."},
 				{"name": "Machine Intelligence Conference", "desc": "MIC is a platform for student to share their work, breaking down institutional barriers, championing diversity and connecting students to industry professionals.", "year": "2018, 2019"},
-				{"name": "student communities", "desc": "MIC, Inc. helped start and grow several student communities at Massachusetts Institute of Technology, Boston University, Harvard College, and SRM Amaravati and Kattankulathur.", "year": "2017-2020"}
+				{"name": "Student communities", "desc": "MIC, Inc. helped start and grow several student communities at Massachusetts Institute of Technology, Boston University, Harvard College, and SRM Amaravati and Kattankulathur.", "year": "2017-2020"}
 			]
 		}
 	},
