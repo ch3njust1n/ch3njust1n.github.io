c015458c2a4fcf4b39e26a6af7f268734bdabfec
Justin Chen <ch3njus@gmail.com>
2020-07-21 12:26:00 -0400

    Updated MIC
    Changed end date

--- src/components/mic.vue
+++ src/components/mic.vue
@@ -27,7 +27,7 @@ export default {
 	data() {
 		return {
 			prog: [
-				{"name": "Machine Intelligence Community, Inc.", "year": "2018-Present", "link":"https://machineintelligence.cc", "desc": "501(c)(3) nonprofit for making education on machine intelligence accessible, effective, and innovative."},
+				{"name": "Machine Intelligence Community, Inc.", "year": "2018-2020", "link":"https://machineintelligence.cc", "desc": "501(c)(3) nonprofit for making education on machine intelligence accessible, effective, and innovative."},
 				{"name": "Machine Intelligence Conference", "desc": "MIC is a platform for students to share their work by breaking down institutional barriers, championing diversity and connecting students to industry professionals.", "year": "2018, 2019"},
 				{"name": "Student communities", "desc": "MIC, Inc. helped start and grow several student communities at Massachusetts Institute of Technology, Boston University, Harvard College, and SRM Institute for Science and Technology at Amaravati and at Kattankulathur.", "year": "2017-2020"}
 			]
